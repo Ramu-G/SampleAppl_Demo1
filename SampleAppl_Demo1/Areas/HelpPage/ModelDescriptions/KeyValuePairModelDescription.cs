3fa2aade2a1acad59ca7b0fb5efa792f240bcde9
Ramu Gangadharan <ramugangadharan@gmail.com>
2017-02-13 20:31:08 +0530

    Intial First Commit

---
+++ SampleAppl_Demo1/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace SampleAppl_Demo1.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ SampleAppl_Demo1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SampleAppl_Demo1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
